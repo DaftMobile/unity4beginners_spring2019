15fc7112d23f8660a44715fd114cac9e3b4f1e56
SzmUryHome <SzmUryHome>
2019-03-08 21:32:18 +0100

    first lesson

---
+++ Assets/Scripts/MainSceneInitializer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MainSceneInitializer : MonoBehaviour
+{
+    [SerializeField] private GameObject shipPrefab;
+
+    private void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject ship = Instantiate(shipPrefab);
+            ship.transform.position = new Vector3(
+                Random.Range(-10, 10),
+                Random.Range(-5, 5),
+                0);
+        }
+    }
+}
---
+++ Assets/Scripts/RotationgScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class RotationgScript : MonoBehaviour
+{
+    [SerializeField] 
+    private float rotationSpeed = 5f;
+
+    [SerializeField] private GameObject otherObject;
+    
+    private RotationgScript rotationgScript;
+
+    private BoxCollider boxy;
+    
+    //wywołuje sie zawsze raz przy stworzeniu obiektu,
+    //odpaleniu projektu, załadowaniu sceny
+    private void Awake()
+    {
+        
+        rotationgScript = otherObject.GetComponent<RotationgScript>();
+        boxy = gameObject.GetComponent<BoxCollider>();
+        int x = 0;
+    }
+    //wywołuje sie zawsze raz przy stworzeniu obiektu,
+    //odpaleniu projektu, załadowaniu sceny jesli obiekt jest aktywny
+    private void Start()
+    {
+    }
+    
+    //Wywołuje się co klatkę
+    //Time delta time uzywany w kazdej symulacji
+    //mowi ile czsau miedzy jedna klatka a druga
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+    
+    //Symulacja fizyki rzeczy w rownych odstepach czasu
+    private void FixedUpdate()
+    {
+        
+    }
+}
